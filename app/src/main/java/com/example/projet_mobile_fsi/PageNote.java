285e0da64fcba8beb13f18780168c197f7add1c3
Maxirrx <max2005.roux@gmail.com>
2025-03-10 20:51:40 +0100

    connection de tout

--- app/src/main/java/com/example/projet_mobile_fsi/PageNote.java
+++ app/src/main/java/com/example/projet_mobile_fsi/PageNote.java
@@ -4,36 +4,31 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
-import java.io.Serializable;
+public class PageNote extends AppCompatActivity {
 
-public class MainActivity extends AppCompatActivity {
-
-    private Button btnconnect;
+    private ImageButton btnburger;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.page_note);
         initialisation();
     }
 
     private void initialisation(){
-        btnconnect = (Button) findViewById(R.id.btnconnect);
+        btnburger = (ImageButton) findViewById(R.id.btnburger);
 
-        btnconnect.setOnClickListener(new View.OnClickListener() {
+        btnburger.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, PageAcceuil.class);
+                Intent intent = new Intent(PageNote.this, Menu.class);
                 startActivity(intent);
-
             }
         });
 
