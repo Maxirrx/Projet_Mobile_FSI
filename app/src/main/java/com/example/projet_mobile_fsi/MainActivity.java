78d01aebf9fbf87586ec3a9a98a257c190fdc618
Maxirrx <max2005.roux@gmail.com>
2025-03-13 11:16:20 +0100

    mqsldkfjlkmqsdjf

--- app/src/main/java/com/example/projet_mobile_fsi/MainActivity.java
+++ app/src/main/java/com/example/projet_mobile_fsi/MainActivity.java
@@ -37,9 +37,11 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void initialisation(){
+
         btnconnect = (Button) findViewById(R.id.btnconnect);
         login = (EditText) findViewById(R.id.login);
         mdp = (EditText) findViewById(R.id.mdp);
+
 
         UtilisateurDAO utilisateurDAO = new UtilisateurDAO(this);
         utilisateurDAO.open();
